dd8f74c4d629d9deb3dd0ee8591e165dd2d33bf4
Lucas Arruda <zatesec@loatecs.com>
2023-06-16 00:27:21 -0300

    chore: reconfigured database scripts and compose

--- arquivos/ModelagemDados/scripts/01-base.sql
+++ arquivos/ModelagemDados/scripts/01-base.sql
@@ -1,4 +1,8 @@
-CREATE DATABASE lanchonete;
+DO $$ BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'lanchonete') THEN
+        CREATE DATABASE lanchonete;
+    END IF;
+END $$;
 
 \c lanchonete
 
