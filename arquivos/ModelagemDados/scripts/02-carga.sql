d66bcec5815cd0fd0abe87ab025954eb8808bbc6
André Luiz Freitas da Silva <andreluiz03@gmail.com>
2023-06-28 19:24:03 -0300

    SQL - Remocao não utilizados

--- arquivos/ModelagemDados/scripts/02-carga.sql
+++ arquivos/ModelagemDados/scripts/02-carga.sql
@@ -1,17 +1,4 @@
 \c lanchonete
 
-
-INSERT INTO "Categoria"("NomeCategoria")
-VALUES ('Lanche');
-
-INSERT INTO "Categoria"("NomeCategoria")
-VALUES ('Acompanhamento');
-
-INSERT INTO "Categoria"("NomeCategoria")
-VALUES ('Bebida');
-
-INSERT INTO "Categoria"("NomeCategoria")
-VALUES ('Sobremesa');
-
 INSERT INTO "Cliente"("NomeCliente")
 VALUES ('Sem identificação');
