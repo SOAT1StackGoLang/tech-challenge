ac9202ad37b44b2c86dacc9a4628073e30a758c5
George Baronheid <baronheid.george@gmail.com>
2023-07-03 22:04:51 -0300

    DeleteOrder done at repository

--- code/internal/core/usecases/authorization.go
+++ code/internal/core/usecases/authorization.go
@@ -1,31 +1,31 @@
 package usecases
 
 import (
-    "context"
-    "github.com/SOAT1StackGoLang/tech-challenge/helpers"
-    "github.com/SOAT1StackGoLang/tech-challenge/internal/core/ports"
-    "github.com/google/uuid"
-    "go.uber.org/zap"
+	"context"
+	"github.com/SOAT1StackGoLang/tech-challenge/helpers"
+	"github.com/SOAT1StackGoLang/tech-challenge/internal/core/ports"
+	"github.com/google/uuid"
+	"go.uber.org/zap"
 )
 
 func validateIsAdmin(log *zap.SugaredLogger, uRepo ports.UsersUseCase, ctx context.Context, userID uuid.UUID) error {
-    admin, err := uRepo.IsUserAdmin(ctx, userID)
-    switch {
-    case err != nil:
-        log.Errorw(
-            "failed checking user is admin",
-            zap.String("userID", userID.String()),
-            zap.Error(err),
-        )
-        return err
-    case !admin:
-        log.Errorw(
-            "unauthorized user",
-            zap.String("id", userID.String()),
-            zap.Error(helpers.ErrUnauthorized),
-        )
-        err = helpers.ErrUnauthorized
-        return err
-    }
-    return nil
+	admin, err := uRepo.IsUserAdmin(ctx, userID)
+	switch {
+	case err != nil:
+		log.Errorw(
+			"failed checking user is admin",
+			zap.String("userID", userID.String()),
+			zap.Error(err),
+		)
+		return err
+	case !admin:
+		log.Errorw(
+			"unauthorized user",
+			zap.String("id", userID.String()),
+			zap.Error(helpers.ErrUnauthorized),
+		)
+		err = helpers.ErrUnauthorized
+		return err
+	}
+	return nil
 }
--- code/internal/repositories/postgres/orders.go
+++ code/internal/repositories/postgres/orders.go
@@ -1,122 +1,136 @@
 package postgres
 
 import (
-    "context"
-    "database/sql"
-    "github.com/SOAT1StackGoLang/tech-challenge/internal/core/domain"
-    "github.com/SOAT1StackGoLang/tech-challenge/internal/core/ports"
-    "github.com/google/uuid"
-    "go.uber.org/zap"
-    "gorm.io/gorm"
-    "time"
+	"context"
+	"database/sql"
+	"github.com/SOAT1StackGoLang/tech-challenge/internal/core/domain"
+	"github.com/SOAT1StackGoLang/tech-challenge/internal/core/ports"
+	"github.com/google/uuid"
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+	"time"
 )
 
 type ordersRepositoryImpl struct {
-    log     *zap.SugaredLogger
-    db      *gorm.DB
-    prodUsc ports.ProductsUseCase
+	log     *zap.SugaredLogger
+	db      *gorm.DB
+	prodUsc ports.ProductsUseCase
 }
 
 const ordersTable = "lanchonete_orders"
 
 func (o *ordersRepositoryImpl) CreateOrder(ctx context.Context, userID, productID uuid.UUID) (*domain.Order, error) {
-    var out *domain.Order
+	var out *domain.Order
 
-    var in *Order
-    in.newFromDomain(userID, productID)
+	var in *Order
+	in.newFromDomain(userID, productID)
 
-    var err error
-    if err = o.db.WithContext(ctx).Table(ordersTable).Create(&in).Error; err != nil {
-        o.log.Errorw(
-            "db failed inserting order",
-            zap.Error(err),
-        )
-        return nil, err
-    }
+	var err error
+	if err = o.db.WithContext(ctx).Table(ordersTable).Create(&in).Error; err != nil {
+		o.log.Errorw(
+			"db failed inserting order",
+			zap.Error(err),
+		)
+		return nil, err
+	}
 
-    if err = o.productsIDToDomainProducts(ctx, out, in); err != nil {
-        return nil, err
-    }
+	if err = o.productsIDToDomainProducts(ctx, out, in); err != nil {
+		return nil, err
+	}
 
-    out = in.toDomain(out.Products)
+	out = in.toDomain(out.Products)
 
-    return out, err
+	return out, err
 }
 
 func (o *ordersRepositoryImpl) productsIDToDomainProducts(ctx context.Context, out *domain.Order, in *Order) error {
-    out.Products = make([]domain.Product, 0, len(in.Products))
-    for _, v := range in.Products {
-        product, err := o.prodUsc.GetProduct(ctx, v)
-        if err != nil {
-            o.log.Errorw(
-                "order db failed getting product by id",
-                zap.String("product_id", v.String()),
-                zap.Error(err),
-            )
-            return err
-        }
+	out.Products = make([]domain.Product, 0, len(in.Products))
+	for _, v := range in.Products {
+		product, err := o.prodUsc.GetProduct(ctx, v)
+		if err != nil {
+			o.log.Errorw(
+				"order db failed getting product by id",
+				zap.String("product_id", v.String()),
+				zap.Error(err),
+			)
+			return err
+		}
 
-        out.Products = append(out.Products, *product)
-    }
-    return nil
+		out.Products = append(out.Products, *product)
+	}
+	return nil
 }
 
 func (o *ordersRepositoryImpl) InsertProductIntoOrder(ctx context.Context, userID, orderID, productID uuid.UUID) (*domain.Order, error) {
-    var out *domain.Order
+	var out *domain.Order
 
-    var in *Order
+	var in *Order
 
-    var err error
-    if err = o.db.WithContext(ctx).Table(ordersTable).
-        Select("*").
-        Where("id = ?", orderID).
-        Where("user_id = ?", userID).
-        Where("deleted_at = ?", nil).
-        First(in).
-        Error; err != nil {
-        o.log.Errorw(
-            "db failed getting order by id and user id",
-            zap.String("order_id", orderID.String()),
-            zap.Error(err),
-        )
-        return nil, err
-    }
+	var err error
+	if err = o.db.WithContext(ctx).Table(ordersTable).
+		Select("*").
+		Where("id = ?", orderID).
+		Where("user_id = ?", userID).
+		Where("deleted_at = ?", nil).
+		First(in).
+		Error; err != nil {
+		o.log.Errorw(
+			"db failed getting order by id and user id",
+			zap.String("order_id", orderID.String()),
+			zap.Error(err),
+		)
+		return nil, err
+	}
 
-    in.Products = append(in.Products, productID)
-    in.UpdatedAt = sql.NullTime{
-        Time:  time.Now(),
-        Valid: true,
-    }
+	in.Products = append(in.Products, productID)
+	in.UpdatedAt = sql.NullTime{
+		Time:  time.Now(),
+		Valid: true,
+	}
 
-    if err = o.db.WithContext(ctx).Table(ordersTable).
-        Updates(in).
-        Where("id = ?", in.ID).
-        Error; err != nil {
-        o.log.Errorw(
-            "db failed inserting product into order",
-            zap.String("order_id", orderID.String()),
-            zap.String("product_id", productID.String()),
-        )
-    }
+	if err = o.db.WithContext(ctx).Table(ordersTable).
+		Updates(in).
+		Where("id = ?", in.ID).
+		Error; err != nil {
+		o.log.Errorw(
+			"db failed inserting product into order",
+			zap.String("order_id", orderID.String()),
+			zap.String("product_id", productID.String()),
+		)
+	}
 
-    if err = o.productsIDToDomainProducts(ctx, out, in); err != nil {
-        return nil, err
-    }
-    out = in.toDomain(out.Products)
+	if err = o.productsIDToDomainProducts(ctx, out, in); err != nil {
+		return nil, err
+	}
+	out = in.toDomain(out.Products)
 
-    return out, err
+	return out, err
 }
 
 func (o *ordersRepositoryImpl) RemoveProductFromOrder(ctx context.Context, userID, orderID, productID uuid.UUID) (*domain.Order, error) {
-    //TODO implement me
-    panic("implement me")
+	//TODO implement me
+	panic("implement me")
 }
 
 func (o *ordersRepositoryImpl) DeleteOrder(ctx context.Context, userID, orderID uuid.UUID) error {
-    //TODO implement me
-    panic("implement me")
+	var err error
+	deletedAt := sql.NullTime{
+		Time:  time.Now(),
+		Valid: true,
+	}
+	if err = o.db.WithContext(ctx).Table(ordersTable).
+		UpdateColumn("deleted_at", deletedAt).
+		Where("user_id", userID).
+		Where("order_id", orderID).
+		Error; err != nil {
+		o.log.Errorw(
+			"db failed deleting order",
+			zap.String("order_id", orderID.String()))
+		return err
+	}
+	return err
 }
 
 func NewPgxOrdersRepository(log *zap.SugaredLogger, db *gorm.DB, productUseCase ports.ProductsUseCase) ports.OrdersRepository {
-    return &ordersRepositoryImpl{log: log, db: db, prodUsc: productUseCase}
+	return &ordersRepositoryImpl{log: log, db: db, prodUsc: productUseCase}
 }
