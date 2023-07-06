989531ab108341f189944c61e623b0142b9204a7
George Baronheid <baronheid.george@gmail.com>
2023-07-05 23:01:37 -0300

    insert product handler done

--- code/internal/handlers/http/products.go
+++ code/internal/handlers/http/products.go
@@ -3,6 +3,7 @@ package http
 import (
 	"context"
 	"fmt"
+	"github.com/SOAT1StackGoLang/tech-challenge/helpers"
 	"github.com/SOAT1StackGoLang/tech-challenge/internal/core/domain"
 	"github.com/SOAT1StackGoLang/tech-challenge/internal/core/ports"
 	"github.com/emicklei/go-restful/v3"
@@ -17,14 +18,20 @@ type (
 		productsUC ports.ProductsUseCase
 	}
 
+	InsertionProduct struct {
+		UserID      string `json:"user_id"`
+		Name        string `json:"name"`
+		Description string `json:"description"`
+		CategoryID  string `json:"category_id"`
+		Price       string `json:"price"`
+	}
+
 	Product struct {
-		ID          string    `json:"id,omitempty"`
-		CreatedAt   time.Time `json:"created_at"`
-		UpdatedAt   time.Time `json:"updated_at,omitempty"`
-		Name        string    `json:"name"`
-		Description string    `json:"description"`
-		CategoryID  string    `json:"category_id"`
-		Price       string    `json:"price"`
+		ID        string    `json:"id,omitempty"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at,omitempty"`
+		DeletedAt time.Time `json:"deleted_at,omitempty"`
+		InsertionProduct
 	}
 )
 
@@ -42,6 +49,36 @@ func (p *Product) fromDomain(product *domain.Product) {
 	p.Description = product.Description
 	p.CategoryID = product.CategoryID.String()
 	p.Price = price
+}
+
+func (p *Product) toDomain() *domain.Product {
+	if p == nil {
+		panic("empty product")
+	}
+
+	return &domain.Product{
+		ID:          helpers.SafeUUIDFromString(p.ID),
+		CategoryID:  helpers.SafeUUIDFromString(p.CategoryID),
+		CreatedAt:   p.CreatedAt,
+		UpdatedAt:   p.UpdatedAt,
+		DeletedAt:   p.DeletedAt,
+		Name:        p.Name,
+		Description: p.Description,
+		Price:       p.Price,
+	}
+}
+
+func (iP *InsertionProduct) toDomain() *domain.Product {
+	if iP == nil {
+		panic("empty product")
+	}
+
+	return &domain.Product{
+		Name:        iP.Name,
+		Description: iP.Description,
+		Price:       iP.Price,
+		CategoryID:  helpers.SafeUUIDFromString(iP.CategoryID),
+	}
 }
 
 func NewProductsHttpHandler(ctx context.Context, productsUC ports.ProductsUseCase, ws *restful.WebService) *ProductsHttpHandler {
@@ -80,7 +117,28 @@ func (pH *ProductsHttpHandler) GetProduct(request *restful.Request, response *re
 
 }
 func (pH *ProductsHttpHandler) InsertProduct(request *restful.Request, response *restful.Response) {
-	panic("implement me")
+	var iProd InsertionProduct
+
+	if err := request.ReadEntity(&iProd); err != nil {
+		_ = response.WriteError(http.StatusBadRequest, err)
+		return
+	}
+
+	uid, err := uuid.Parse(iProd.UserID)
+	if err != nil {
+		_ = response.WriteError(http.StatusBadRequest, err)
+		return
+	}
+
+	product, err := pH.productsUC.InsertProduct(pH.ctx, uid, iProd.toDomain())
+	if err != nil {
+		_ = response.WriteError(http.StatusInternalServerError, err)
+		return
+	}
+
+	var prod Product
+	prod.fromDomain(product)
+	_ = response.WriteAsJson(prod)
 }
 func (pH *ProductsHttpHandler) UpdateProduct(request *restful.Request, response *restful.Response) {
 	panic("implement me")
